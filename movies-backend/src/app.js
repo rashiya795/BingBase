1b635c8e07f85e0df93803e831e31ea9d3ad8c5d
rashiya795 <133627374+rashiya795@users.noreply.github.com>
2025-11-04 19:08:53 +0530

    Updated schema & migrations and app.js with CORS

--- movies-backend/src/app.js
+++ movies-backend/src/app.js
@@ -8,7 +8,27 @@ import "dotenv/config"
 
 const app =express()
 
-app.use(cors());
+const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(",") || [];
+
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true); 
+
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
+
+
+
 app.use(express.json())
 
 app.use("/api/media",mediaRoutes)
