9214085ca071db018fff8be8d586d5e593caa6a1
rashiya795 <133627374+rashiya795@users.noreply.github.com>
2025-11-05 12:06:01 +0530

    Update CORS allowed origins

--- movies-backend/src/app.js
+++ movies-backend/src/app.js
@@ -11,21 +11,12 @@ const app =express()
 const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(",") || [];
 
 
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true); 
-
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
-
+app.use(cors({
+  origin: allowedOrigins,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+}));
 
 
 
