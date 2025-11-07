e9ab00791c315232dff6460e75fe0b9d453e1947
rashiya795 <133627374+rashiya795@users.noreply.github.com>
2025-11-07 12:44:52 +0530

    update the vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,8 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  build: {
-    outDir: 'dist', // <-- make sure Vercel knows where to look
-  },
+  plugins: [react(),tailwindcss(),],
 })
