332e75181e75978f2bd68719cfdf040060d656a0
rashiya795 <133627374+rashiya795@users.noreply.github.com>
2025-11-07 07:39:02 +0530

    updated vercel.json

--- movies-backend/src/routes/authRotes.js
+++ movies-backend/src/routes/authRotes.js
@@ -5,7 +5,5 @@ const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
-router.get("/test", (req, res) => {
-  res.json({ message: "✅ Auth API is working" });
-});
+
 export default router;
